9755a316550697450d71e9ec309dfdaff94dc3d1
Paul Zhutovsky <paul.zhutovsky@gmail.com>
2017-03-03 17:20:25 +0100

    fixes for the training: initialization of the network

--- train_network.py
+++ train_network.py
@@ -26,15 +26,21 @@ def evaluate(data, y_data, id_to_take, network, batch_size=25):
     accuracy_batches = np.zeros(num_batches)
 
     for i_batch, offset in enumerate(xrange(0, num_examples, batch_size)):
-        stdout.write('/r {}/{}'.format(i_batch + 1, num_batches))
+        stdout.write('\r {}/{}'.format(i_batch + 1, num_batches))
         stdout.flush()
         end = np.minimum(offset+batch_size, num_examples)
         id_minibatch = id_to_take[offset:end]
-        batch_x, batch_y = data[id_minibatch, :, :, :, :], y_data[id_minibatch]
+        batch_x, batch_y = atleast_5d(data[id_minibatch, :, :, :, :]), np.array(y_data[id_minibatch])
         accuracy = sess.run(network.get_performance(), feed_dict={network.input: batch_x, network.label: batch_y})
         accuracy_batches[i_batch] = accuracy
     print
     return accuracy_batches
+
+
+def atleast_5d(data):
+    if len(data.shape) != 5:
+        return data[np.newaxis, :, :, :, :]
+    return data
 
 
 def train_network(data, y, id_train, id_valid, id_test, network, save_path, batch_size=25,
@@ -50,9 +56,10 @@ def train_network(data, y, id_train, id_valid, id_test, network, save_path, batc
     accuracy_test = np.zeros((num_batches_test, num_epochs))
     accuracy_valid = np.zeros((num_batches_valid, num_epochs))
 
+    train_op = network.get_training_function()
+
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-
 
         print "Training..."
         print
@@ -63,15 +70,16 @@ def train_network(data, y, id_train, id_valid, id_test, network, save_path, batc
             np.random.shuffle(id_train)
 
             for id_batch, offset in enumerate(xrange(0, num_train_examples, batch_size)):
-                stdout.write('/r {}/{}'.format(id_batch + 1, num_batches_train))
+                stdout.write('\r {}/{}'.format(id_batch + 1, num_batches_train))
                 stdout.flush()
                 end = np.minimum(offset + batch_size, num_train_examples)
                 id_minibatch = id_train[offset:end]
                 id_minibatch = np.sort(id_minibatch)
 
-                batch_x, batch_y = data[id_minibatch, :, :, :, :], y[id_minibatch]
-                sess.run(network.get_training_function(), feed_dict={network.input: batch_x, network.label: batch_y})
+                batch_x, batch_y = atleast_5d(data[id_minibatch, :, :, :, :]), np.array(y[id_minibatch])
+                sess.run(train_op, feed_dict={network.input: batch_x, network.label: batch_y})
 
+            print 'Validation...'
             accuracy_valid[:, id_epoch] = evaluate(data, y, id_valid, network, batch_size=batch_size)
             accuracy_train[:, id_epoch] = evaluate(data, y, id_train, network, batch_size=batch_size)
             accuracy_test[:, id_epoch] = evaluate(data, y, id_test, network, batch_size=batch_size)
@@ -100,13 +108,12 @@ def iterate_and_train(hdf5_file_path, save_path, batch_size=25):
                       batch_size=batch_size, num_epochs=20)
 
 
-def init_network(batch_size):
-    n_classes = 2
+def init_network(batch_size, n_classes=2):
     batch_size = batch_size
     nx, ny, nz = 91, 109, 91
 
     conv_params = [{'shape': (8, 8, 8, 1, 6),
-                    'strides': (1, 2, 2, 2, 1)},
+                    'strides': (1, 5, 5, 5, 1)},
                    {'shape': (5, 5, 5, 6, 16),
                     'strides': (1, 2, 2, 2, 1)}
                    ]
@@ -115,7 +122,7 @@ def init_network(batch_size):
                        {'ksize': (1, 2, 2, 2, 1),
                         'strides': (1, 2, 2, 2, 1)}
                        ]
-    fc_params = [{'shape': (1280, 120)},
+    fc_params = [{'shape': (16, 120)},
                  {'shape': (120, 84)},
                  {'shape': (84, n_classes)}]
 
@@ -125,9 +132,23 @@ def init_network(batch_size):
     return DeepPsychNet(X, y, n_classes=n_classes, conv_layers_params=conv_params,
                         max_pool_layers_params=max_pool_params, fc_layers_params=fc_params)
 
+def test(hdf5_file, batch_size):
+    network = init_network(batch_size)
+    with h5py.File(hdf5_file, 'r') as hdf5_file:
+        data = hdf5_file['dataT1']
+        data = data[:batch_size, :, :, :, :]
+
+        prediction = tf.nn.softmax(network.network)
+        with tf.Session() as sess:
+            sess.run(tf.variables_initializer(network.variable_list))
+            new_predictions = sess.run(prediction, feed_dict={network.input: data})
+
+    return new_predictions
+
 
 if __name__ == '__main__':
     hdf5_file = '/media/paul/kaggle/dataHDF5/abide.hdf5'
     save_path = '/media/paul/kaggle/dataHDF5/DeepPsychNet'
-    batch_size = 2
+    batch_size = 25
+    # tmp = test(hdf5_file, batch_size)
     iterate_and_train(hdf5_file_path=hdf5_file, save_path=save_path, batch_size=batch_size)
