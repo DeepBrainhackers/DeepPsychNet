aa647ab0d28b9db1f505f4533cd3afd9996f2390
Paul Zhutovsky <paul.zhutovsky@gmail.com>
2017-03-10 13:07:22 +0100

    forgot to commit rotation

--- rotate_translate_brain.py
+++ rotate_translate_brain.py
@@ -16,28 +16,32 @@ def rotate_brain(image_data, angle, axis, affine_matrix):
     angle is the rotation angle in degrees
     axis is the rotation axis 0,1 or 2"""
     
-    angle = math.radians(angle)
+    angle = np.deg2rad(angle)
+
     if axis == 1:
-        rotation = np.array([[1,0,0,0], [0,math.cos(angle),-1*math.sin(angle),0],
-             [0, math.sin(angle),math.cos(angle),0],
-              [0,0,0,1]])
+        rotation = np.array([[1, 0, 0, 0],
+                             [0, np.cos(angle), -1*np.sin(angle), 0],
+                             [0, np.sin(angle), np.cos(angle), 0],
+                             [0, 0, 0, 1]])
     elif axis==2:
-        rotation = np.array([[math.cos(angle),0,math.sin(angle),0],[0,1,0,0],
-             [-1*math.sin(angle),0,math.cos(angle),0],[0,0,0,1]])
+        rotation = np.array([[np.cos(angle), 0, np.sin(angle), 0],
+                             [0, 1, 0, 0],
+                             [-1*np.sin(angle), 0, np.cos(angle), 0],
+                             [0, 0, 0, 1]])
     elif axis==3: 
-        rotation = np.array([[math.cos(angle),-1*math.sin(angle),0,0],
-                 [math.sin(angle),math.cos(angle),0,0],
-             [0,0,1,0],
-              [0,0,0,1]])
+        rotation = np.array([[np.cos(angle), -1*np.sin(angle), 0, 0],
+                             [np.sin(angle), np.cos(angle), 0, 0],
+                             [0, 0, 1, 0],
+                             [0, 0, 0, 1]])
+    else:
+        raise RuntimeError('axis has to be 1, 2 or 3. {} was given'.format(axis))
     
-    new_affine = np.dot(affine_matrix,rotation)    
+    new_affine = affine_matrix.dot(rotation)
     
     #offset in the center
     center = 0.5*np.array(image_data.shape)
     offset = center - center.dot(new_affine[:3,:3])        
-    rotated_data = affine_transform(image_data,new_affine[0:3,0:3:].T,
-                                    offset=offset,order=0)
-
+    rotated_data = affine_transform(image_data, new_affine[0:3,0:3:].T, offset=offset, order=0)
     
     return rotated_data, new_affine
 
