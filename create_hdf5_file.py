a520b8bc9daf851377629c8aaf0a6ace2bbd4bd3
Paul Zhutovsky <paul.zhutovsky@gmail.com>
2017-03-10 11:50:50 +0100

    make hdf5 file creation more flexible by allowing for different patterns for the image path

--- create_hdf5_file.py
+++ create_hdf5_file.py
@@ -14,10 +14,11 @@ def ensure_folder(folder_dir):
         os.makedirs(folder_dir)
 
 
-def create_hdf5_file(directory_data, sub_folders=('ASD', 'CON'), save_directory='hdf5_data', save_path='data.hdf5'):
+def create_hdf5_file(directory_data, sub_folders=('ASD', 'CON'), save_directory='hdf5_data', save_path='data.hdf5',
+                     nifit_ending='*T1_shft_res.nii'):
     ensure_folder(save_directory)
 
-    path_niftis, n_subj, subj_id = get_niftis(directory_data, sub_folders)
+    path_niftis, n_subj, subj_id = get_niftis(directory_data, sub_folders, nifti_ending=nifit_ending)
     labels_subj = np.concatenate((np.ones(n_subj[0]), np.zeros(n_subj[1])))
 
     affine_dtype, affine_shape, data_shape = get_affine_shape(path_niftis)
@@ -65,6 +66,7 @@ def get_niftis(directory_data, sub_folders, nifti_ending='*T1_shft_res.nii'):
         num_subj[i_sub_folder] = len(tmp)
         path_def += tmp
 
+
     return path_def, num_subj, np.array(subj_id)
 
 
@@ -73,7 +75,9 @@ def run():
     sub_folders = ('ASD', 'CON')
     save_folder = '/media/paul/kaggle/dataHDF5'
     save_file = 'abide.hdf5'
-    create_hdf5_file(directory_data, sub_folders, save_directory=save_folder, save_path=save_file)
+    img_pattern = '*T1_shft_res.nii'
+    create_hdf5_file(directory_data, sub_folders, save_directory=save_folder, save_path=save_file,
+                     nifit_ending=img_pattern)
 
 
 if __name__ == '__main__':
