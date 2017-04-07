3fb809da5a67de05053249c9ea6db3e352cf28b2
Paul Zhutovsky <paul.zhutovsky@gmail.com>
2017-04-07 11:25:54 +0200

    integrate the keras model into the training

--- train_network.py
+++ train_network.py
@@ -7,7 +7,8 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
-from deepPsychNet import DeepPsychNet
+# from deepPsychNet import DeepPsychNet
+from deepPsychNet_keras import init_network as get_keras_network
 from imageTransformer3d import ImageTransformer3d
 
 
@@ -26,27 +27,7 @@ def run():
 
 
 def init_network(batch_size=None, n_classes=2):
-    nx, ny, nz = 91, 109, 91
-
-    conv_params = [{'shape': (8, 8, 8, 1, 6),
-                    'strides': (1, 5, 5, 5, 1)},
-                   {'shape': (5, 5, 5, 6, 16),
-                    'strides': (1, 2, 2, 2, 1)}
-                   ]
-    max_pool_params = [{'ksize': (1, 2, 2, 2, 1),
-                        'strides': (1, 2, 2, 2, 1)},
-                       {'ksize': (1, 2, 2, 2, 1),
-                        'strides': (1, 2, 2, 2, 1)}
-                       ]
-    fc_params = [{'shape': (16, 120)},
-                 {'shape': (120, 84)},
-                 {'shape': (84, n_classes)}]
-
-    X = tf.placeholder(tf.float32, (batch_size, nx, ny, nz, 1))
-    y = tf.placeholder(tf.int32, (None))
-
-    return DeepPsychNet(X, y, n_classes=n_classes, conv_layers_params=conv_params,
-                        max_pool_layers_params=max_pool_params, fc_layers_params=fc_params)
+    return get_keras_network(n_classes=n_classes)
 
 
 def create_train_validation_test_set(data, y, num_test=100, num_valid=100):
@@ -68,7 +49,9 @@ def evaluate(data, y_data, id_to_take, network, affine, batch_size=25):
     num_examples = id_to_take.size
     sess = tf.get_default_session()
     num_batches = int(np.ceil(num_examples/float(batch_size)))
-    accuracy_batches = np.zeros(num_batches)
+    num_performance_metrics = len(network.metrics_names)
+
+    metrics_batches = np.zeros((num_batches, num_performance_metrics))
 
     image_iterator_transformer = ImageTransformer3d(data_obj=data, affine=affine, y_label=y_data,
                                                     id_data=id_to_take, batch_size=batch_size,
@@ -79,11 +62,11 @@ def evaluate(data, y_data, id_to_take, network, affine, batch_size=25):
         stdout.write('\r {}/{}'.format(i_batch + 1, num_batches))
         stdout.flush()
 
-        accuracy = sess.run(network.get_performance(), feed_dict={network.input: batch_x, network.label: batch_y,
-                                                                  network.keep_dims: 1.})
-        accuracy_batches[i_batch] = accuracy
+        metrics = network.test_on_batch(batch_x, batch_y)
+        metrics_batches[i_batch, :] = metrics
+
     print
-    return accuracy_batches
+    return metrics_batches
 
 
 def train_network(data, y, affine, id_train, id_valid, id_test, network, save_path, model_folder, model_name,
@@ -95,55 +78,61 @@ def train_network(data, y, affine, id_train, id_valid, id_test, network, save_pa
     num_batches_test = int(np.ceil(id_test.size/float(batch_size)))
     num_batches_valid = int(np.ceil(id_valid.size/float(batch_size)))
 
-    accuracy_train = np.zeros((num_batches_train_validation, num_epochs))
-    accuracy_test = np.zeros((num_batches_test, num_epochs))
-    accuracy_valid = np.zeros((num_batches_valid, num_epochs))
+    num_metrics = len(network.metrics_names)
 
-    train_op = network.get_training_function()
+    metrics_train = np.zeros((num_batches_train_validation, num_epochs, num_metrics))
+    metrics_test = np.zeros((num_batches_test, num_epochs, num_metrics))
+    metrics_valid = np.zeros((num_batches_valid, num_epochs, num_metrics))
+
     model_save = osp.join(save_path, model_folder)
 
     if not osp.exists(model_save):
         os.makedirs(model_save)
 
-    with tf.Session() as sess:
 
-        sess.run(tf.global_variables_initializer())
+    print "Training..."
+    print
 
-        print "Training..."
+    for id_epoch in xrange(num_epochs):
+        print 'EPOCH {}/{}:'.format(id_epoch + 1, num_epochs)
+
+        image_iterator_transformer = ImageTransformer3d(data_obj=data, affine=affine, y_label=y,
+                                                        id_data=id_train, batch_size=batch_size,
+                                                        num_augmentation_set=num_augmentation, shuffle=True)
+        image_generator = image_iterator_transformer.iter()
+
+        for id_batch, (batch_x, batch_y, affine_train) in enumerate(image_generator):
+            stdout.write('\r {}/{}'.format(id_batch + 1, num_batches_train))
+            stdout.flush()
+
+            network.train_on_batch(batch_x, batch_y)
+
         print
+        print 'Validation...'
+        metrics_valid[:, id_epoch, :] = evaluate(data, y, id_valid, network, affine, batch_size=batch_size)
+        metrics_train[:, id_epoch, :] = evaluate(data, y, id_train, network, affine, batch_size=batch_size)
+        metrics_test[:, id_epoch, :] = evaluate(data, y, id_test, network, affine, batch_size=batch_size)
 
-        for id_epoch in xrange(num_epochs):
+        print
+        print 'Training: '
+        print_metrics(metrics_train[:, id_epoch, :].mean(axis=0), network.metrics_names)
+        print 'Validation: '
+        print_metrics(metrics_valid[:, id_epoch, :].mean(axis=0), network.metrics_names)
+        print 'Test: '
+        print_metrics(metrics_test[:, id_epoch, :].mean(axis=0), network.metrics_names)
 
-            image_iterator_transformer = ImageTransformer3d(data_obj=data, affine=affine, y_label=y,
-                                                            id_data=id_train, batch_size=batch_size,
-                                                            num_augmentation_set=num_augmentation, shuffle=True)
-            image_generator = image_iterator_transformer.iter()
+        network.save(osp.join(model_save, model_name + '_epoch_{}.h5'.format(id_epoch + 1)))
 
-            for id_batch, (batch_x, batch_y, affine_train) in enumerate(image_generator):
-                stdout.write('\r {}/{}'.format(id_batch + 1, num_batches_train))
-                stdout.flush()
+    np.savez_compressed(osp.join(model_save, 'metrics_model.npz'), metrics_train=metrics_train,
+                        metrics_valid=metrics_valid, metrics_test=metrics_test, metrics_names=network.metrics_names)
+    print 'Model saved!'
 
-                sess.run(train_op, feed_dict={network.input: batch_x, network.label: batch_y,
-                                              network.keep_dims: 0.5})
 
-            print
-            print 'Validation...'
-            accuracy_valid[:, id_epoch] = evaluate(data, y, id_valid, network, affine, batch_size=batch_size)
-            accuracy_train[:, id_epoch] = evaluate(data, y, id_train, network, affine, batch_size=batch_size)
-            accuracy_test[:, id_epoch] = evaluate(data, y, id_test, network, affine, batch_size=batch_size)
-
-            print
-            print "EPOCH {}/{}: Training Acc: {:.3f}; Validation Acc = {:.3f}; Test Acc = {:.3f}".format(id_epoch + 1,
-                                                                                                         num_epochs,
-                                                                                                         accuracy_train[:, id_epoch].mean(),
-                                                                                                         accuracy_valid[:, id_epoch].mean(),
-                                                                                                         accuracy_test[:, id_epoch].mean())
-            print
-            saver.save(sess, osp.join(model_save, model_name + '_epoch_{}.ckpt'.format(id_epoch + 1)))
-
-        np.savez_compressed(osp.join(model_save, 'accuracies_data.npz'), accuracy_train=accuracy_train,
-                            accuracy_valid=accuracy_valid, accuracy_test=accuracy_test)
-        print 'Model saved!'
+def print_metrics(metrics_array, metric_names):
+    print_str = ''
+    for i in xrange(len(metric_names)):
+        print_str += '{}: {:.2f} '.format(metric_names[i], metrics_array[i])
+    print print_str
 
 
 def iterate_and_train(hdf5_file_path, save_path, model_folder='model', model_name='model.ckpt', batch_size=25,
