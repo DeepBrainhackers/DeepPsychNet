923f66fce6d0620019f81fe13ff3dabdcb5f8f20
Paul Zhutovsky <paul.zhutovsky@gmail.com>
2017-04-29 15:31:28 +0200

    improved readability for resnet architecture

--- resnet_architechture.py
+++ resnet_architechture.py
@@ -17,52 +17,56 @@ def conv_block(x, num_conv, base_name):
     return x
 
 
+def conv_bn_act(x, filters, kernel_size=(3, 3, 3), strides=(1, 1, 1), base_name='conv-layer', num_layer=1):
+    x = Conv3D(filters=filters, kernel_size=kernel_size, strides=strides, padding='same',
+               name='{}-conv{}'.format(base_name, num_layer))(x)
+    x = BatchNormalization(name='{}-bn{}'.format(base_name, num_layer))(x)
+    x = Activation('relu', name='{}-relu{}'.format(base_name, num_layer))(x)
+    return x
+
+
+def bn_act(x, base_name='bn_act', num_layer=1):
+    x = BatchNormalization(name='{}-bn{}'.format(base_name, num_layer))(x)
+    x = Activation('relu', name='{}-relu{}'.format(base_name, num_layer))(x)
+    return x
+
+
 def ResNet():
     input_layer = Input(batch_shape=(None, 91, 109, 91, 1))
 
-    x = Conv3D(filters=32, kernel_size=(3, 3, 3), strides=(1, 1, 1), name='init-conv1')(input_layer)
-    x = BatchNormalization(name='init-bn1')(x)
-    x = Activation('relu', name='init-relu1')(x)
+    x = conv_bn_act(input_layer, filters=32, base_name='1st', num_layer=1)
+    x = conv_bn_act(x, filters=32, base_name='2nd', num_layer=2)
 
-    x = Conv3D(filters=32, kernel_size=(3, 3, 3), strides=(1, 1, 1), name='init-conv2')(x)
-    x = BatchNormalization(name='init-bn2')(x)
-    x = Activation('relu', name='init-relu2')(x)
-
-    x = Conv3D(filters=64, kernel_size=(3, 3, 3), strides=(2, 2, 2), name='init-conv3')(x)
+    x = Conv3D(filters=64, kernel_size=(3, 3, 3), strides=(2, 2, 2), padding='same', name='3rd-conv3')(x)
+    # 1st block of 2 convolutions/batch-norm/relu followed by addiing of outputs and another block
     block = conv_block(x, 64, 'block1')
-
     add1 = Add(name='add1')([x, block])
-
     block2 = conv_block(add1, 64, 'block2')
     add2 = Add(name='add2')([add1, block2])
+    x = bn_act(add2, base_name='bn-act', num_layer=1)
 
-    x = BatchNormalization(name='2nd-bn1')(add2)
-    x = Activation('relu', name='2nd-relu1')(x)
-    x = Conv3D(filters=64, kernel_size=(3, 3, 3), strides=(2, 2, 2), name='2nd-conv')(x)
-
+    x = Conv3D(filters=64, kernel_size=(3, 3, 3), strides=(2, 2, 2), padding='same', name='2nd-conv')(x)
+    # 2nd block of 2 convolutions/batch-norm/relu followed by addiing of outputs and another block
     block3 = conv_block(x, 64, 'block3')
     add3 = Add(name='add3')([x, block3])
-
     block4 = conv_block(add3, 64, 'block4')
     add4 = Add(name='add4')([add3, block4])
+    x = bn_act(add4, base_name='bn-act', num_layer=2)
 
-    x = BatchNormalization(name='3rd-bn1')(add4)
-    x = Activation('relu', name='3rd-relu1')(x)
-    x = Conv3D(filters=128, kernel_size=(3, 3, 3), strides=(2, 2, 2), name='3rd-conv')(x)
-
+    x = Conv3D(filters=128, kernel_size=(3, 3, 3), strides=(2, 2, 2), padding='same', name='3rd-conv')(x)
+    # 3rd block of 2 convolutions/batch-norm/relu followed by addiing of outputs and another block
     block5 = conv_block(x, 128, 'block5')
     add5 = Add(name='add5')([x, block5])
-
     block6 = conv_block(add5, 128, 'block6')
     add6 = Add(name='add6')([add5, block6])
 
-    x = MaxPool3D(pool_size=(7, 7, 7))(add6)
+    # Finalizing the network
+    x = MaxPool3D(pool_size=(7, 7, 7), name='mp3d1')(add6)
     x = Flatten(name='flatten')(x)
-    x = Dense(units=128, activation='relu')(x)
+    x = Dense(units=128, activation='relu', name='fc1')(x)
     output = Dense(units=2, activation='softmax')(x)
 
     model = Model(input_layer, output)
-
     model.summary()
 
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', balanced_accuracy])
