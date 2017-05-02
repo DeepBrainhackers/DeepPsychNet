99f81a25638d64b6261059432fc8922d52157651
Paul Zhutovsky <paul.zhutovsky@gmail.com>
2017-05-02 11:02:06 +0200

    kernel size changed from (3, 3, 3) to (7, 7, 7); learning rate of adam is now 1e-6; filter maps size changed (first 2 layers 64 filters, following two block 32)

--- resnet_architechture.py
+++ resnet_architechture.py
@@ -1,19 +1,20 @@
 from keras.layers import Conv3D, BatchNormalization, Activation, MaxPool3D, Dense, Input, Add, Flatten
 from keras.models import Model
+from keras.optimizers import Adam
 from deepPsychNet_keras import balanced_accuracy
 from multi_gpu import make_parallel
 
 
-def conv_block(x, num_conv, base_name):
+def conv_block(x, num_conv, base_name, kernel_size=(3, 3, 3)):
     x = BatchNormalization(name='{}-bn1'.format(base_name))(x)
     x = Activation('relu', name='{}-relu1'.format(base_name))(x)
 
-    x = Conv3D(filters=num_conv, kernel_size=(3, 3, 3), padding='same', strides=(1, 1, 1),
+    x = Conv3D(filters=num_conv, kernel_size=kernel_size, padding='same', strides=(1, 1, 1),
                name='{}-conv1'.format(base_name))(x)
     x = BatchNormalization(name='{}-bn2'.format(base_name))(x)
     x = Activation('relu', name='{}-relu2'.format(base_name))(x)
 
-    x = Conv3D(filters=num_conv, kernel_size=(3, 3, 3), padding='same', strides=(1, 1, 1),
+    x = Conv3D(filters=num_conv, kernel_size=kernel_size, padding='same', strides=(1, 1, 1),
                name='{}-conv2'.format(base_name))(x)
     return x
 
@@ -35,34 +36,34 @@ def bn_act(x, base_name='bn_act', num_layer=1):
 def ResNet():
     input_layer = Input(batch_shape=(None, 91, 109, 91, 1))
 
-    x = conv_bn_act(input_layer, filters=32, base_name='1st', num_layer=1)
-    x = conv_bn_act(x, filters=32, base_name='2nd', num_layer=2)
+    x = conv_bn_act(input_layer, filters=64, kernel_size=(7, 7, 7), base_name='1st', num_layer=1)
+    x = conv_bn_act(x, filters=64, kernel_size=(7, 7, 7), base_name='2nd', num_layer=2)
 
-    x = Conv3D(filters=64, kernel_size=(3, 3, 3), strides=(2, 2, 2), padding='same', name='3rd-conv3')(x)
+    x = Conv3D(filters=32, kernel_size=(7, 7, 7), strides=(2, 2, 2), padding='same', name='3rd-conv3')(x)
     # 1st block of 2 convolutions/batch-norm/relu followed by addiing of outputs and another block
-    block = conv_block(x, 64, 'block1')
+    block = conv_block(x, 32, 'block1', kernel_size=(7, 7, 7))
     add1 = Add(name='add1')([x, block])
-    block2 = conv_block(add1, 64, 'block2')
+    block2 = conv_block(add1, 32, 'block2', kernel_size=(7, 7, 7))
     add2 = Add(name='add2')([add1, block2])
     x = bn_act(add2, base_name='bn-act', num_layer=1)
 
-    x = Conv3D(filters=64, kernel_size=(3, 3, 3), strides=(2, 2, 2), padding='same', name='2nd-conv')(x)
+    x = Conv3D(filters=32, kernel_size=(7, 7, 7), strides=(2, 2, 2), padding='same', name='2nd-conv')(x)
     # 2nd block of 2 convolutions/batch-norm/relu followed by addiing of outputs and another block
-    block3 = conv_block(x, 64, 'block3')
+    block3 = conv_block(x, 32, 'block3', kernel_size=(7, 7, 7))
     add3 = Add(name='add3')([x, block3])
-    block4 = conv_block(add3, 64, 'block4')
+    block4 = conv_block(add3, 32, 'block4', kernel_size=(7, 7, 7))
     add4 = Add(name='add4')([add3, block4])
     x = bn_act(add4, base_name='bn-act', num_layer=2)
 
-    x = Conv3D(filters=128, kernel_size=(3, 3, 3), strides=(2, 2, 2), padding='same', name='3rd-conv')(x)
+    x = Conv3D(filters=128, kernel_size=(7, 7, 7), strides=(2, 2, 2), padding='same', name='3rd-conv')(x)
     # 3rd block of 2 convolutions/batch-norm/relu followed by addiing of outputs and another block
-    block5 = conv_block(x, 128, 'block5')
+    block5 = conv_block(x, 128, 'block5', kernel_size=(7, 7, 7))
     add5 = Add(name='add5')([x, block5])
-    block6 = conv_block(add5, 128, 'block6')
+    block6 = conv_block(add5, 128, 'block6', kernel_size=(7, 7, 7))
     add6 = Add(name='add6')([add5, block6])
 
     # Finalizing the network
-    x = MaxPool3D(pool_size=(7, 7, 7), name='mp3d1')(add6)
+    x = MaxPool3D(pool_size=(5, 5, 5), name='mp3d1')(add6)
     x = Flatten(name='flatten')(x)
     x = Dense(units=128, activation='relu', name='fc1')(x)
     output = Dense(units=2, activation='softmax')(x)
@@ -73,7 +74,7 @@ def ResNet():
     model = make_parallel(model, 2)
     model.summary()
 
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy', balanced_accuracy])
+    model.compile(optimizer=Adam(lr=1e-6), loss='categorical_crossentropy', metrics=['accuracy', balanced_accuracy])
     return model
 
 
